0f815cca1e37e7c98594975e1ba47b12fc9d1eb0
Umesh <umeshtamang129@gmail.com>
2025-01-26 17:49:17 +0545

    decouple with config

--- imageuploadproject/imageuploadproject/settings.py
+++ imageuploadproject/imageuploadproject/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4yio40sl%#c+gal6rysq5tm9!%*8s)_3-jv1wc22)8wgejy-6l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
@@ -65,6 +66,17 @@ WSGI_APPLICATION = 'imageuploadproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# connect mysql database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  config('DB_NAME'),
+        'USER':  config('DB_USER'),
+        'PASSWORD': '',
+        'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT')
+}
+}
 
 
 # Password validation
