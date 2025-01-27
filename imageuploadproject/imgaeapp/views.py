ec4d2beaa42b9390e1b4bb0016195bf737ae4a10
Umesh <umeshtamang129@gmail.com>
2025-01-27 08:57:14 +0545

    logic views for post data and files

--- imageuploadproject/imgaeapp/views.py
+++ imageuploadproject/imgaeapp/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile
 
 # Create your views here.
 
 def profile(request):
+    if request.method =="POST" and request.FILES:
+        name=request.POST["profile_name"] #frontend bata aako
+        print(name)
+        image=request.FILES["profile_image"] #frontend bata aako
+        print(image)
+        user=Profile(profile_name=name,profile_image=image) #model=frontend part
+        user.save()
+        return redirect('profile')
     return render(request,'profile.html')
